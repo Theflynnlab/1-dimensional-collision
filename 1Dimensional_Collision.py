7ca5b8fe548b5f69808a13460521b6472dd7ed8b
Theflynnlab <73565445+Theflynnlab@users.noreply.github.com>
2020-10-29 22:15:53 -0700

    Add files via upload

--- 1Dimensional_Collision.py
+++ 1Dimensional_Collision.py
@@ -5,5 +5,16 @@ def OneD_Collison(m1,v1,m2,v2,e):
         eq1=Eq(m1*v1 + m2*v2, m1*v1f + m2*v2f)
         eq2=Eq(1/2*m1*v1**2 + 1/2*m2*v2**2, 1/2*m1*v1f**2 + 1/2*m2*v2f**2)
         sol = solve([eq1,eq2],[v1f, v2f])  #need to drop the v1=v1f and v2=v2f case
-    print(sol)
-OneD_Collison(2,3,4,-2,1)
+    solution=[]
+    for i in range(len(sol)):           #flatten the list.
+        for j in range(len(sol[i])):
+            solution.append("{:.2f}".format(sol[i][j]))
+    for i in range(len(solution)):
+        solution[i]=float(solution[i])
+    solution.remove(v1)
+    solution.remove(v2)
+    ans= "first mass moving {} m/s and second mass moving {} m/s after collision".format(solution[0],solution[1])
+
+
+    print(ans)
+OneD_Collison(6,8,4,3,1)
